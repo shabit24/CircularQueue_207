3adb5e74f47d4b2d2eef3d1d25529c4fefa0506d
shabit24 <hossain.shabit24@gmail.com>
2023-07-15 20:53:23 +0700

    insert function

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -15,5 +15,29 @@ public:
 
 	}
 
+void insert() {
+
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// Cek apakah antrian penuh
+
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+
+			cout << "\nQueue overflow\n";
+			return;
+
+		}
+
+		// Cek apakah antrian kosong
+
+		if (FRONT == -1) {
+
+			FRONT = 0;
+			REAR = 0;
+
+		}
 
 
